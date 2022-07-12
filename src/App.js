d8de9fa59645dfd89629cf68d27553fa3f9c8fcf
tdudkowski <tdudkowski@gmail.com>
2022-07-13 00:22:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ListComponent from "./ListComponent";
 
 function App() {
+
+  // const url = 'http://127.0.0.1:3001/api'
+  const url = 'http://node.tdudkowski.usermd.net/api'
+
+  const [data, setData] = useState(null);
+  const [openAdd, setOpenAdd] = useState(false);
+  const [textareaAdd, setTextareaAdd] = useState("");
+
+  useEffect(() => {
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => setData(data))
+   }, [data]);
+
+  // ADD
+  const handleItemCreate = async (e) => {
+    e.preventDefault()
+
+    if (textareaAdd && (typeof JSON.parse(textareaAdd) == "object")) {
+      try {
+        let res = await fetch(url, {
+          method: "POST",      
+          body: JSON.stringify(textareaAdd),
+        })  
+        if (res.status === 200) {
+          setTextareaAdd("")
+          setOpenAdd(false)
+          return res;
+        } else {
+          console.log("Some error occured " + res.status);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Book App</h1>
+      
+      <h2>Add a Book</h2>
+      
+      <p>Here add a book</p>
+
+{openAdd ? <div>
+  <form onSubmit={(e) => handleItemCreate(e)}>
+  <textarea  rows="10" cols="80" value={textareaAdd} onChange={(e) => setTextareaAdd(e.target.value)}></textarea>
+  <hr />
+  <button type="submit" onClick={(e) => console.log(textareaAdd)}>Add</button> | <button onClick={() => setOpenAdd(false)}>Close add a book</button>
+  </form></div> : <div><button onClick={() => setOpenAdd(true)}>Open to add a book</button></div>}
+      
+
+      <h2>Book List</h2>
+
+      {data ?  <ul>
+        {data.map(el => (<ListComponent key={el._id} id={el._id} data={data} />)) }
+        </ul> : "not loaded yet"}
+      
+      </div>
   );
 }
 
---
+++ src/ListComponent.js
@@ -0,0 +1,74 @@
+
+import { useState  } from "react";
+
+const url = 'http://127.0.0.1:3001/api'
+
+const ListComponent = (props) => {
+
+
+    const thisElement = props.data.filter(el =>  el._id === props.id)
+    const thisElementArr = []
+    Object.entries(thisElement[0]).map(el => thisElementArr.push(el))
+
+    const [openUpdate, setOpenUpdate] = useState(false);
+    const [openDelete, setOpenUDelete] = useState(false);
+    const [content, setContent] = useState(thisElement[0]);
+
+    // INPUT CHANGE
+    const handleInputChange = (e, id) => {
+        const keyname = Object.keys(content)[id]
+        content[keyname] = e.target.value
+        setContent(content)
+        console.log(content)
+            }
+
+    // ITEM UPDATE
+    const handleItemUpdate = async () => {
+    try {
+        const res = await fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(content)
+    });
+    if (res.status === 201) {
+    setOpenUpdate(false)
+    console.log(res.body)
+    return res;
+  } else {
+    console.log("Some error occured " + res.status);
+  }
+} catch (err) {
+    console.log("ERR!");
+  console.log(err);
+} 
+}
+
+    // ITEM DELETE
+    const handleItemDelete = async (elid) => {
+    try {
+      const res = await fetch(url, {
+        method: "DELETE",      
+        body: JSON.stringify(elid),
+      });
+      if (res.status === 200) {
+        setOpenUDelete(false);
+        return res;
+      } else { console.log("Some error occured " + res.status); }
+    } catch (err) { console.log(err); } 
+  }
+
+    return (
+      <li>
+      {thisElement[0].author} "<em>{thisElement[0].title}</em>" ({thisElement[0].year})
+      
+      {openUpdate ? <div>
+                <ul>
+                    {Object.entries(thisElement[0]).map((el, id) =>  <li key={id}><em>{el[0]}: </em> <input defaultValue={el[1]} editable="true" onChange={(e) => handleInputChange(e, id)} /></li>)}
+                </ul>
+        <button onClick={() => handleItemUpdate(content)}>Update</button> | <button onClick={() => setOpenUpdate(false)}>Close update</button></div> :  <div><button onClick={() => setOpenUpdate(true)}>Show update</button></div>}
+
+        {openDelete ? <div>Do you want to delete this item? <button onClick={() => handleItemDelete(thisElement[0]['_id'])}>Delete</button> | <button onClick={() => setOpenUDelete(false)}>Close delete</button></div> :  <div><button onClick={() => setOpenUDelete(true)}>Show delete</button></div>}
+      </li>
+    )
+  }
+
+export default ListComponent;
